5c929a23283c6d23be72c4a7da91e3824962a89d
xpernalete <xpernalete@cdev.global>
2017-10-27 07:34:40 -0600

    add search and crud library-song

--- app/Http/Controllers/LibrarySongController.php
+++ app/Http/Controllers/LibrarySongController.php
@@ -16,47 +16,94 @@ class LibrarySongController extends Controller
 
     public function index(Request $request)
     {
+        try {
 
+            $songs = Song::where($request->all())->get()->take(isset($limit) ? $limit : 20)->toArray();
+
+            if (is_array($songs) && !empty($songs)) {
+
+                return $this->sendResponse($songs, 'songs', 'Completed Created');
+
+            }
+
+            return $this->sendResponse(null, 'songs', 'Song No Exits ');
+        } catch (\Exception $e) {
+
+            Log::info('Error Showed songs ' . $e->getMessage());
+        }
 
     }
 
 
     public function store(Request $request)
     {
+        try {
 
-        if (trim(strtolower($request->import)) == "spotify" && isset($request->idSpotify)) {
+            if (trim(strtolower($request->import)) == "spotify" && isset($request->idSpotify)) {
 
-            $spotify = new SpotifyService();
-            $result  = $spotify->trackDetails($request->idSpotify);
-            $this->saveSongs($result);
-            return $this->sendResponse($result, 'songs' , 'Completed Import');
+                $spotify = new SpotifyService();
+                $result = $spotify->trackDetails($request->idSpotify);
+                $this->saveSongs($result);
+                return $this->sendResponse($result, 'songs', 'Completed Import');
 
+            }
+
+            $songs = $this->createNewSong($request->all());
+            if (is_array($songs)) {
+
+                return $this->sendResponse($songs, 'songs', 'Completed Created');
+
+            }
+
+            return $this->sendResponse(null, 'songs', 'Missing Parameter: ' . $songs);
+        } catch (\Exception $e) {
+
+            Log::info('Error Showed songs ' . $e->getMessage());
         }
-
-        $songs = $this->createNewSong($request->all());
-        if(is_array($songs)){
-
-            return $this->sendResponse($songs, 'songs' , 'Completed Created');
-
-        }
-
-        return $this->sendResponse(null, 'songs' , 'Missing Parameter: '.$songs);
 
     }
 
 
-    public function show ($id){
+    public function show($id)
+    {
+        try {
+            $song = Song::find($id)->toArray();
 
-        $song = Song::find($id)->toArray();
+            if (is_array($song)) {
 
-        if(is_array($song)){
+                return $this->sendResponse($song, 'songs', 'Completed Search');
 
-            return $this->sendResponse($song, 'songs' , 'Completed Search');
+            }
+
+            return $this->sendResponse(null, 'songs', 'Song No Exits');
+        } catch (\Exception $e) {
+
+            Log::info('Error Showed songs ' . $e->getMessage());
 
         }
 
-        return $this->sendResponse(null, 'songs' , 'Missing Parameter: '.$song);
+    }
 
+    public function destroy($id)
+    {
+
+        try {
+            $song = Song::find($id);
+
+            if (empty($song)) {
+
+                return $this->sendResponse(null, 'songs', 'Song no exits ');
+
+            }
+
+            $song->delete($id);
+
+            return $this->sendResponse($song, 'songs', 'Completed Deleted');
+
+        } catch (\Exception $e) {
+
+            Log::info('Error deleted songs ' . $e->getMessage());
+        }
 
     }
 
@@ -66,47 +113,52 @@ class LibrarySongController extends Controller
         try {
 
             if ((!is_null($result) || !empty($result))) {
-                $songExit = Song::where('idspotify',$result[0]->id )->first();
+                $songExit = Song::where('idspotify', $result[0]->id)->first();
 
-                if(empty($songExit)){
+                if (empty($songExit)) {
 
-                $song = new Song();
-                $song->url = $result[0]->url;
-                $song->idspotify = $result[0]->id;
-                $song->songname = $result[0]->songname;
-                $song->artistid = $result[0]->artistid;
-                $song->artistname = $result[0]->artistname;
-                $song->albumid = $result[0]->albumid;
-                $song->albumname = $result[0]->albumname;
-                $song->save();
+                    $song = new Song();
+                    $song->url = $result[0]->url;
+                    $song->idspotify = $result[0]->id;
+                    $song->songname = $result[0]->songname;
+                    $song->artistid = $result[0]->artistid;
+                    $song->artistname = $result[0]->artistname;
+                    $song->albumid = $result[0]->albumid;
+                    $song->albumname = $result[0]->albumname;
+                    $song->save();
                 }
 
             }
         } catch (\Exception $e) {
 
-            Log::info('Error save songs ' . $e->getMessage());
+            Log::info('Error saved songs ' . $e->getMessage());
 
         }
 
     }
 
-    private function createNewSong($params){
+    private function createNewSong($params)
+    {
+        try {
 
-        $requires = array(
-            "url","songname","artistname",
-        );
-        foreach ($requires as $require){
+            $requires = array(
+                "url", "songname", "artistname",
+            );
+            foreach ($requires as $require) {
 
-            if(!array_key_exists($require , $params)){
+                if (!array_key_exists($require, $params)) {
 
-                return $require;
+                    return $require;
+                }
+
             }
+            $song = Song::create($params);
 
+            return $song->toArray();
+        } catch (\Exception $e) {
+
+            Log::info('Error create songs ' . $e->getMessage());
         }
-        $song = Song::create($params);
-
-        return $song->toArray();
-
     }
 
 
