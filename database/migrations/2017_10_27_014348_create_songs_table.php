328ce252fffa661b5dfdc9a804fe2c21ef0082a0
xpernalete <xpernalete@cdev.global>
2017-10-26 19:47:11 -0600

    add migrations songs

---
+++ database/migrations/2017_10_27_014348_create_songs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('songname');
+            $table->string('artistid');
+            $table->string('artistname');
+            $table->string('albumid');
+            $table->string('albumname');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
